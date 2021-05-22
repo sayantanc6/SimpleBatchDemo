430538f863c95da5e08056948b9a70f6d3fd3acb
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-05-22 09:15:52 +0530

    Add files via upload
    SimpleBatchDemo

---
+++ BatchDemo/src/main/java/dummy/Processor.java
@@ -0,0 +1,11 @@
+package dummy;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class Processor implements ItemProcessor<String, String> {
+
+	@Override
+	public String process(String item) throws Exception {
+		return item.toUpperCase();
+	}
+}
---
+++ BatchDemo/src/main/java/dummy/config/BatchConfig.java
@@ -0,0 +1,45 @@
+package dummy.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import dummy.JobCompletionListener;
+import dummy.Processor;
+import dummy.Reader;
+import dummy.Writer;
+ 
+@Configuration
+public class BatchConfig {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job processJob() {
+		return jobBuilderFactory.get("processJob")
+				.incrementer(new RunIdIncrementer()).listener(listener())
+				.flow(orderStep1()).end().build();
+	}
+	
+	@Bean
+	public Step orderStep1() {
+		return stepBuilderFactory.get("orderStep1").<String, String> chunk(1)
+				.reader(new Reader()).processor(new Processor())
+				.writer(new Writer()).build();
+	}
+	
+	@Bean
+	public JobExecutionListener listener() {
+		return new JobCompletionListener();
+	}
+}
